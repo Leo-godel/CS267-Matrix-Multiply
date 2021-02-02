f834851a421faf052c8afb2df3a6156ef8e42c76
Leo-godel <23514493+Leo-godel@users.noreply.github.com>
2021-02-03 00:49:18 +0800

    Add files via upload

--- naive.c
+++ naive.c
@@ -1,13 +1,15 @@
 void square_dgemm(int n, double* A, double* B, double* C) {
     int P,Q;
     double R;
-    for (int k = 0; k < n; ++k) {
-        P = k * n;
-        for (int j = 0; j < n; ++j) {
-            Q = j * n;
+    for (int j = 0; j < n; ++j) {
+        Q = j * n;
+        for (int k = 0; k < n; ++k) {
             R = B[k + Q];
+            P = k * n;
             for (int i = 0; i < n; ++i)
                 C[i + Q] += A[i + P] * R;
         }
     }
+
+
 }
